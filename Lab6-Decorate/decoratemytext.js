14f6c12da941d2a2027ca3de8a9d0218b02ffa6f
BEREKET ABRAHAM <bereketmichael741@gmail.com>
2019-07-28 03:08:45 -0500

    Lab6 Decorate

---
+++ Lab6-Decorate/decoratemytext.js
@@ -0,0 +1,61 @@
+function hello(){
+    alert("Hello, World!");
+}
+function interval(){
+    var id = setInterval(enlargeText,1000);
+    setTimeout(clearInterval,5000,id);
+}
+function enlargeText(){
+    // document.getElementById("textarea").style.fontSize="24pt";
+    var prop = window.getComputedStyle(document.getElementById("textarea")).fontSize;
+    newProp = parseInt(prop) + 2 + "pt";
+    document.getElementById("textarea").style.fontSize=newProp;
+}
+
+function boldText(){
+    if(document.getElementById("bling").checked == true){
+        document.getElementById("textarea").style.fontWeight="bold";
+        document.getElementById("textarea").style.color="green";
+        document.getElementById("textarea").style.textDecoration="underline";
+        document.body.style.backgroundImage='url("http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg")';
+    }else{
+        document.getElementById("textarea").style.fontWeight="normal";
+        document.getElementById("textarea").style.color="black";
+        document.getElementById("textarea").style.textDecoration="none";
+    }
+}
+function pig(){
+    var words = document.getElementById("textarea").value;
+    var splittedword = words.split(/\s+/);
+    var result = "";
+    for(var i = 0; i<splittedword.length; i++){
+        if(splittedword[i].charAt(0).match("[aeoiuAEOIU]")){
+            changed = splittedword[i] + "ay";
+        }else{
+            var word = splittedword[i];
+            for(var j=0;j<word.length;j++){
+                if(word[j].match("[aeoiuAEOIU]")){
+                    var changed =  word.substring(j) + word.substring(0,j) +  "ay";
+                    break;
+                }
+
+            }
+        }
+        result += changed + " ";
+
+    }
+    document.getElementById("textarea").value = result;
+
+}
+function markov(){
+    var words=document.getElementById("textarea").value;
+    var arr = words.split(/\s+/);
+    var result = "";
+    for(let i=0; i< arr.length; i++){
+        if(arr[i].length>=5){
+            arr[i] = "Malkovich";
+        }
+        result +=arr[i] + " ";
+    }
+    document.getElementById("textarea").value= result;
+}
