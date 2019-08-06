8092d20d5abc022e14a8ab2fd1c97a2b63ef5806
BEREKET ABRAHAM <bereketmichael741@gmail.com>
2019-08-05 21:51:41 -0500

    CSS style sheet

--- Lab10-Maze/maze.js
+++ Lab10-Maze/maze.js
@@ -1,9 +1,8 @@
 var win = true;
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#start").click(
-        function()
-        {
+        function () {
             $("#status").text("Good Luck.");
             $("div .boundary").removeClass("youlose");
             startGame();
@@ -11,41 +10,36 @@ $(document).ready(function(){
         });
 });
 
-function startGame()
-{
+function startGame() {
     win = true;
 
     $(".boundary").hover(
-        function()
-        {
+        function () {
             loose();
         });
 
     $("#end").click(
-        function()
-        {
-            if(win)
-            {
+        function () {
+            if (win) {
                 $("#status").text("You WIN! : Click S to Play Again");
                 endGame();
             }
         });
 
     $("#maze").mouseleave(
-        function()
-        {
+        function () {
             loose();
         });
 
 }
 
-function loose(){
-    win=false;
+function loose() {
+    win = false;
     $("div .boundary").addClass("youlose");
     $("#status").text("You LOSE! : Click 'S' to Retry ");
 }
 
-function endGame(){
+function endGame() {
     $("div .boundary").removeClass("youlose");
     $('.boundary').unbind('hover');
     $("#maze").unbind('mouseleave');
